b9c2c5c87b96ab0cc747694ee3329da08b6d3dda
Saulo <saulojar@gmail.com>
2025-05-09 20:00:48 -0300

    feat: add lambda

---
+++ infra/lambda.tf
@@ -0,0 +1,15 @@
+resource "aws_lambda_function" "hello_world_lambda" {
+  function_name = "hello_world_lambda"
+  role          = aws_iam_role.lambda_execution_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.10"
+  filename      = "lambda.zip"
+
+  source_code_hash = filebase64sha256("lambda.zip")
+
+  environment {
+    variables = {
+      ENV = "dev"
+    }
+  }
+}
