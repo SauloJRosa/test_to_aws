84f22396d502344aaff6dc8c62821ad93674a4ab
Saulo <saulojar@gmail.com>
2025-05-09 22:21:04 -0300

    fix: organizando arquivos

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -3,9 +3,9 @@ resource "aws_lambda_function" "hello_world_lambda" {
   role          = aws_iam_role.lambda_execution_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.10"
-  filename      = "lambda.zip"
+  filename      = "output/lambda_function.zip"
 
-  source_code_hash = filebase64sha256("lambda.zip")
+  source_code_hash = filebase64sha256("output/lambda_function.zip")
 
   environment {
     variables = {
