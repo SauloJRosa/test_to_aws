65dd9850a589722ca4689f0ce75a8ec3b09e08c9
Saulo <saulojar@gmail.com>
2025-05-09 22:39:37 -0300

    fix: ajuste lambda.tf

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "hello_world_lambda" {
   role          = aws_iam_role.lambda_execution_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.10"
-  filename      = "output/lambda_function.zip"
+  filename      = "../output/lambda_function.zip"
 
   source_code_hash = filebase64sha256("output/lambda_function.zip")
 
