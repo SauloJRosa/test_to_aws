e4ae4c75a3df478525ae3e2ae9c3e98570e8efc1
Saulo <saulojar@gmail.com>
2025-05-09 21:38:53 -0300

    fix: debug buildspec

--- check_buildspec.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "=== Conteúdo de /codebuild/readonly/buildspec.yml ==="
-cat /codebuild/readonly/buildspec.yml
-echo "=== Conteúdo de /codebuild/output/src*/src/buildspec.yml ==="
-cat /codebuild/output/src*/src/buildspec.yml
